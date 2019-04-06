095fe26ac2c46f94d3c1c8d36e3795dc5b650583
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-04-06 18:35:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string == isLowerCase) {
+  if (string == string.isLowerCase) {
     return "I can\'t hear you!"
   }
   
-  else if (string == isUpperCase)
+  else if (string == string.isUpperCase) {
+    return "YES INDEED!"
+  }
 }
