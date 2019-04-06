2423057e5a05a67bb8c9462c26b74349d3225f4d
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-04-06 18:38:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
   else if (string == string.isUpperCase) {
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma." {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
